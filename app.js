57fede3eed620c4ccc5c34697b3e77475ef00573
hungdg42304 <hungdoan370@gmail.com>
2026-09-23 01:36:06 +0000

    Test Jenkins Poll SCM trigger

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("CI/CD deployment successful!");
+  res.send("CI/CD automated by Jenkins Poll SCM!");
 });
 
 app.listen(port, () => {
